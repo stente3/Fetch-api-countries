d7722164efa313bd621ec93260d89714c6f09c15
stente3 <stentem0103@gmail.com>
2022-02-04 20:02:11 -0500

    the dark mode template was added

--- source/js/view.js
+++ source/js/view.js
@@ -1,3 +1,4 @@
+// Filter Section
 let filter = document.querySelector(".filter");
 let options = document.querySelectorAll(".option");
 let defaulOption = document.querySelector(".filter__defaul-option .option p");
@@ -11,4 +12,15 @@ options.forEach((e) =>{
         defaulOption.textContent = e.target.textContent;
     })
 })
+
+// Dark Mode
+let body = document.querySelector(".body");
+let buttonMode = document.querySelectorAll(".dark-mode--change");
+
+buttonMode.forEach(element =>{
+    element.addEventListener("click", (e) =>{
+        console.log(e.target);
+    })
+})
+
 
