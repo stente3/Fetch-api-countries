8fd652f6d9eabc21b84ea387c6de6440abf7c1ac
stente3 <stentem0103@gmail.com>
2022-02-18 09:26:34 -0500

    Feat(Fetch API): Cards are added from fetch api
    Removed cards template and now cards are added from fetch api, also fixed some layout bugs.

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,33 @@
 fetch("https://restcountries.com/v2/all")
     .then(response => response.json())
-    .then(data => createImg(data) )
+    .then(data => createCard(data) )
 // Variables
-let main = document.querySelector(".main");
-let card = document.querySelector(".card");
+let mainCard = document.querySelector(".main");
+let currentNumber = 20;
 
 // Functions
+function createCard(data){
+    for(let i = 0; i < currentNumber; i++){
+        let card = document.createElement("div");
+        card.classList.add("card");
+        mainCard.appendChild(card);
+        card.innerHTML = ` 
+        <div class="card-container__img">
+                        <img src="${data[i].flag}" class="card__image"></img>
+                    </div>
+                    <div class="information">
+                        <h3 class="information__heading"> ${data[i].name} 
+                        </h3>
+                        <p class="information__paragraph">
+                            <span>Population: </span> ${data[i].population}
+                        </p>
+                        <p class="information__paragraph">
+                            <span>Region: </span> ${data[i].region}
+                        </p>
+                        <p class="information__paragraph">
+                            <span>Capital: </span>${data[i].capital}
+                        </p>
+                    </div>
+        `;
+    }
+}
