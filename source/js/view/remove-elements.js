99a67f61d9a0c6d48c1f53842e5a9a5ff1b70360
stente3 <stentem0103@gmail.com>
2022-03-31 18:29:59 -0500

    Feat(Remove-Elements): the Remove-element file was added
    With this file we can hide and remove different parts of our code

---
+++ source/js/view/remove-elements.js
@@ -0,0 +1,35 @@
+// Variables
+let mainSection = document.querySelector(".main");
+let footer = document.querySelector(".footer");
+let filter = document.querySelector(".filter-section");
+
+
+/* Hide arrow down */
+function hideArrow(){
+    if(!footer.classList.contains("hide")){
+        footer.classList.add("hide");
+    }
+}
+/* Hide mainSection */
+function hideMainSection(){
+    mainSection.classList.add("hide");
+    footer.classList.add("hide");
+}
+
+/* Hide all since filter section */
+function hideFilterSection(){
+    filter.classList.add("hide");
+    mainSection.classList.add("hide");
+    footer.classList.add("hide");
+}
+/* Remove cards of main section and hide arrow down */
+function rmMainSection(){
+    while(mainSection.children.length > 0){
+        let i = 0;
+        mainSection.children[i].remove();
+        i++;
+    }
+    hideArrow();
+}
+// Exports
+export { hideFilterSection, hideMainSection, rmMainSection };
